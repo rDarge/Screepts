c64bdda080de91e464388edea3575ebd5479e48f
Ryan Darge <ryan.darge@gmail.com>
2016-08-10 19:35:31 -0400

    another small change

--- main.js
+++ main.js
@@ -689,7 +689,8 @@ module.exports.loop = function () {
                     },{
                         role: 'remote_hauler_g',
                         count: 1,
-                        stats: [CARRY,CARRY,CARRY,CARRY,MOVE,MOVE],
+                        stats: [CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,
+                                MOVE,MOVE],
                         pickupBehavior: [pickupEnergy],
                         depositBehavior: [depositStructure],
                         memory: {
